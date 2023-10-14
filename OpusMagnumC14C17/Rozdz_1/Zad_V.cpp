ae447b3225944c281fdc90359f0d61061bc01726
Tomek Janowski <janek.tomek@gmail.com>
2023-10-14 19:19:50 +0200

    dodatnie uprzednio stworzonej tresci zad5

---
+++ OpusMagnumC14C17/Rozdz_1/Zad_V.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+#include<vector>
+#include<string>
+
+using namespace std;
+
+//Class Template START
+template<typename...Classes>
+class Postac_gry: public Classes
+//Class Template STOP
+
+int main(){
+    cout << "Opus 14/17 Rozdz_1 Zad_5: " << __PRETTY_FUNCTION__ <<endl;
+
+}
