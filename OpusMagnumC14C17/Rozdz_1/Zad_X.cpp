5a6ef72e80af3c823c4cc1741f2f0d2ac320ddba
Tomek Janowski <janek.tomek@gmail.com>
2023-10-28 20:35:59 +0200

    stworzenie zad 10

---
+++ OpusMagnumC14C17/Rozdz_1/Zad_X.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+template <typename T, typename... Args>
+void fun(T& pierwszy,Args&...arg){
+cout << "Pracuje konstruktor: " << __PRETTY_FUNCTION__ << endl;
+}
+
+
+int main(){
+cout << "Wykonujemy: " << "OpusMagMisjaC14C14_Rozdz1_Zad10"<< endl;
+
+
+}
