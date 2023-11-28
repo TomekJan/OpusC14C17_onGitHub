9c7cea821282d12cf3e707b065aaccd27a371f40
Tomek Janowski <janek.tomek@gmail.com>
2023-11-28 20:30:46 +0100

    R2_zad2 zrobione bez zbednych komentarzy

--- OpusMagnumC14C17/Rozdz_2/Zad_01.cpp
+++ OpusMagnumC14C17/Rozdz_2/Zad_01.cpp
@@ -6,20 +6,36 @@ using namespace std;
 
 /*Funkcja ma wypisywaæ na ekranie przys³an¹ jej liczbê.  Ma tê liczbê wypisaæ w zapisie binarnym,
 ale tak, aby  opuœciæ ewentualne nieznacz¹ce zera.  */
-void binarnie(short int liczba){
+template<typename T>
+void binarnie(T liczba){
     cout << "Dziala: " << __PRETTY_FUNCTION__ << endl;
     bitset<sizeof(liczba)*8> liczba_bitowo(liczba);
     cout << "liczba bitowo: " << liczba_bitowo << endl;
+
     string liczba_string;
     liczba_string=liczba_bitowo.to_string();
     const char* liczba_char =liczba_string.c_str();
     cout << "liczba jako c-string: " << liczba_char << endl;
 
+    //Wypisywanie info START
+    /*
     cout << "Rusza iteracja po petli." << endl;
-    int i{0};
-    while ((liczba_char[i]) || (i<100)){
-        cout << "i=" << i++ << ", liczba[i]=" << liczba_char[i] << " || ";
+    cout << "Ilosc znakow liczba_bitowo.size(): " << liczba_bitowo.size() << endl;
+    cout << "Ilosc znakow liczba_string.size(): " << liczba_string.size() << endl;
+
+
+    for (int i=0; i<liczba_bitowo.size(); i++){
+        cout << "liczba_nr_" << i << " to: " << liczba_bitowo[i] << " | ";
     }
+    */
+    //Wypisywanie info STOP
+
+    cout << "\n Wydruk z f-cji while:\n";
+    // Skip trailing '0':
+    int i=(liczba_bitowo.size()-1);
+    while (liczba_bitowo[i] == 0) { i--;  };
+    do (cout << liczba_bitowo[i], i--);
+    while (i>=0);
 }
 
 
